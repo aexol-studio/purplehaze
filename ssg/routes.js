5e31edc91213e50700af94c7524fb3eaab7c7080
aexol <aexol@users.noreply.github.com>
2022-02-16 17:29:57 +0000

    deploy: 40331da1a523c5c87b25e215492ede51ba66f23a

--- ssg/routes.js
+++ ssg/routes.js
@@ -1,5 +1,14 @@
 // src/ssg/routes.ts
 var routes = {
+  "page/routes": "/page/routes",
+  "page/react": "/page/react",
+  "page/import-from-url": "/page/import-from-url",
+  "page/pages": "/page/pages",
+  "page/markdown": "/page/markdown",
+  "page/graphql": "/page/graphql",
+  "page/env-variables": "/page/env-variables",
+  "page/configuration": "/page/configuration",
+  "page/changelog": "/page/changelog",
   "index": "/index"
 };
 export {
